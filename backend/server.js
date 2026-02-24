20cb5cb54122c652ebda3b161bb6f995451a36ab
Ori Katz <orikatz99@walla.co.il>
2026-02-24 11:48:26 +0200

    setup express server and project structure

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 4000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "API is running" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
